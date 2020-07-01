f1f57f33c4bc95bd5de90d156f593007e315c934
Jagriti Maurya <jamaurya@in.ibm.com>
2020-07-01 17:20:49 +0530

    silder fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,6 @@ import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 import { MenuBarComponent } from "./components/menu-bar/menu-bar.component";
 import { RegisterComponent } from "./components/register/register.component";
 import { BlogComponent } from "./components/blog/blog.component";
-import { AboutTabComponent } from "./components/about-tab/about-tab.component";
 import { HomeComponent } from './components/home/home.component';
 
 const routes: Routes = [
@@ -24,10 +23,7 @@ const routes: Routes = [
     path: "blog",
     component: BlogComponent,
   },
-  {
-    path: "about-tab",
-    component: AboutTabComponent,
-  },
+  
 ];
 
 @NgModule({
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,28 +7,23 @@ import {IonSlides} from '@ionic/angular';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
-  @ViewChild(IonSlides) slides: IonSlides;
   tab:string;
 
-  slideData:[
-    "assets/image/kaarwaa.n.jpg",
-    "assets/image/imgk1.jpg",
-    "assets/image/kaarwaa.n.jpg"
-  ]
-  
+  slideOptions = {
+    initialSlide: 0,
+    speed: 700,
+  };
+
   ionViewWillEnter(){
     this.tab = 'about';
   }
-  segmentChanged(ev: any) {
-    console.log('Segment changed', ev);
-  }
+ 
   constructor() { }
 
-  ngOnInit() {
-    // this.slides.startAutoplay().then(()=>{})
+  slidesDidLoad(slides: IonSlides) {
+    slides.startAutoplay();
   }
-  // slideOpts = {
-  //   initialSlide: 1,
-  //   speed: 400
-  // };
+
+  ngOnInit() {}
+
 }
